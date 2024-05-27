eaaefba7b0d61f41d6112c59b86e30ff54e4b008
Hoang Nam Nguyen <HoangNam.Nguyen01@edu.sait.ca>
2024-05-27 08:48:45 -0600

    The first coding

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default Page(){
+    return (
+        <div>
+        <p>My name is Nam</p>
+        <Link href="https://github.com/namnguyen050330/cprg306-assignments">My GitHub repository</Link>
+        </div>
+    );
+}
